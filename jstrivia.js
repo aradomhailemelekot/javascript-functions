b24fee6a5c0a0665c5b4d7ebaf5e0ec883a1c880
Your Name <you@example.com>
2019-03-05 16:56:18 -0500

    New HTML which override css, general questions are listed. dom formatting next.

---
+++ jstrivia.js
@@ -0,0 +1,64 @@
+const formDiv = document.getElementById("form");
+const submitButton = document.getElementById("submitButton")
+let nameValue = document.getElementById("nameInput").value;
+const gameDiv = document.getElementById("game")
+
+const formSubmit = function(e) {
+    console.log("click")
+    submitButton.addEventListener("click", formSubmit)
+    // startGame = function(){
+}
+
+nameValue=document.getElementById("nameInput").value
+
+formDiv.style.display="none"
+
+console.log(nameValue)
+
+startGame();
+gameDiv.style.display="block"
+
+// Question 1
+
+    // Write a "function" that prints "Hello World"
+
+    let nameHello = alert
+    prompt("Type hello")
+    let qOne  = ("Type Hello")
+    let where = function (){
+        
+        if (where == Hello || HELLO || hello || HEllo){
+        alert("Correct!")
+        }
+        else{
+        alert("Close! The correct answer is hello")    
+        }
+    }
+
+
+
+   let nameBye = alert
+    prompt("Type Bye")
+    let qTwo  = ("Type Bye")
+    let where = function (){
+        
+        if (where == "Bye" || "BYE" || "bye" || "BYe"){
+        alert("Correct!")
+        }
+        else{
+        alert("Close! The correct answer is bye")    
+        }
+    }
+
+    let nameBye = alert
+    prompt("Type Bye")
+    let qThree  = ("Type Functions")
+    let where = function (){
+        
+        if (where == Functions || FUNCTIONS || functions || FUnctions){
+        alert("Correct!")
+        }
+        else {
+        alert("Close! The correct answer is bye")    
+        }
+    }
